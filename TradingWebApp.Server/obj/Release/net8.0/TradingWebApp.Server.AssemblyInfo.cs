d1d5891de9a8252b4b5b05146c5cca483c34d604
Van Nguyen <vdnnguyen94@gmail.com>
2025-01-24 19:53:31 -0500

    add .vs/ for .gitignore

--- TradingWebApp.Server/obj/Release/net8.0/TradingWebApp.Server.AssemblyInfo.cs
+++ TradingWebApp.Server/obj/Release/net8.0/TradingWebApp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradingWebApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da1eea0aa7042e9473802a24207caa284ac6f4e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradingWebApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradingWebApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
